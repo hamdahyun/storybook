92bc595497a9c691f3ead9219910a6e70919c862
dahyun Ham <dahuyn1@gmail.com>
2021-08-05 21:35:56 +0900

    storybook basic practice

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Box from "./components/Box/Box";
+import Button from "./components/Button/Button";
+import Grid from "./components/Grid/Grid";
+
+function App() {
+  return (
+    <div className="App" style={{}}>
+        
+        <Button btnType="ty2" />
+    </div>
+  );
+}
+
+export default App;
